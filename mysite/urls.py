662e52511c19e90214842cc556078541b7db3fd6
svitlanadykun <svitlana.dkn@gmail.com>
2019-01-22 18:10:46 +0200

    added comments, logout button

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logut/$', views.LogoutView.as_view(next_page='/'), name='logout'),
     url(r'', include('blog.urls')),
-    url(r'^accounts/login/$', views.LoginView.as_view(), name='login')
-]
+
+    ]
